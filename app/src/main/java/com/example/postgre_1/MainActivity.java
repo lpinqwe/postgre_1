a396db446e032ed284140b959f01ae78755e9c6d
Volodar <gorelikovva@gmail.com>
2023-07-29 22:54:30 +0200

    add stringBuilder class add lamdafunction

--- app/src/main/java/com/example/postgre_1/MainActivity.java
+++ app/src/main/java/com/example/postgre_1/MainActivity.java
@@ -11,13 +11,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
 public class MainActivity extends Activity implements SensorEventListener {
     private SensorManager mSensorManager;
-
+    List<Integer> sensorArrayList = new ArrayList<>();
     StringBuilder mInfo = new StringBuilder();
     StringBuilder mAcc = new StringBuilder("Акселерометр:");
     StringBuilder mLAcc = new StringBuilder("Линейный акселерометр:");
@@ -28,6 +30,15 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorArrayList.add(Sensor.TYPE_ACCELEROMETER);
+        sensorArrayList.add(Sensor.TYPE_GRAVITY);
+        sensorArrayList.add(Sensor.TYPE_GYROSCOPE);
+        sensorArrayList.add(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
+        sensorArrayList.add(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensorArrayList.add(Sensor.TYPE_ROTATION_VECTOR);
+        sensorArrayList.add(Sensor.TYPE_SIGNIFICANT_MOTION);
+        sensorArrayList.add(Sensor.TYPE_STEP_COUNTER);
+        sensorArrayList.add(Sensor.TYPE_STEP_DETECTOR);
 
     }
 
@@ -55,52 +66,12 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     @Override
     protected void onResume() {
-       /* mSensorManager = (SensorManager) this
-                .getSystemService(Context.SENSOR_SERVICE);
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),
-                SensorManager.SENSOR_DELAY_UI);
-
-        mSensorManager.registerListener(this, mSensorManager
-                        .getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE),
-                SensorManager.SENSOR_DELAY_UI);
-
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE),
-                SensorManager.SENSOR_DELAY_UI);
-*/
-        Object[] sensors = new Object()[30];
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),//3
-                SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER_UNCALIBRATED),//6
-                SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),//3
-                SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),//3
-                SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED),//6
-                SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION),//3
-                SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),//4
-                SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_SIGNIFICANT_MOTION),//1
-                SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER),//1
-                SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR),//1
-                SensorManager.SENSOR_DELAY_GAME);
-
+        sensorArrayList.parallelStream().forEach(
+                sensor -> {
+                    mSensorManager.registerListener(this,
+                            mSensorManager.getDefaultSensor(sensor),
+                            SensorManager.SENSOR_DELAY_UI);
+                });
         super.onResume();
     }
 
@@ -114,12 +85,16 @@ public class MainActivity extends Activity implements SensorEventListener {
     public void onSensorChanged(SensorEvent event) {
         mInfo = new StringBuilder();
 
+
+
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             mAcc = new StringBuilder("акселерометр:");
             mAcc.append("\n"+event.values[0]+"\n");
             mAcc.append(     event.values[1]+"\n");
             mAcc.append(     event.values[2]+"\n");
+
         }
+
 
         mInfo.append(String.format("%1$s\n", mAcc));
 
---
+++ app/src/main/java/com/example/postgre_1/StringDataBuilder.java
@@ -0,0 +1,15 @@
+package com.example.postgre_1;
+
+public class StringDataBuilder {
+public String defString;
+
+    public StringDataBuilder(String defString) {
+        this.defString = defString;
+    }
+    public StringDataBuilder() {
+        defString = "";
+    }
+    public void AddData(Object[] arr){
+
+    }
+}
